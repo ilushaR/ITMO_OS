e7e9c3ff0f0d84bc0e859f6588d8a6692544d3d5
rogulinilya <rogulinilya@gmail.com>
2020-09-21 00:01:35 +0300

    fix task 10

--- lab1/script10.sh
+++ lab1/script10.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-man bash | sed "s/\s/\n/g" | sort | uniq -c | sort -nr | awk 'length($2) > 3 {print $2}' | head -3
+man bash | sed "s/[.,()?!;=]/ /g"  | sed "s/\s/\n/g" | awk '{print tolower($0)}' | sort | uniq -c | sort -nr | awk 'length($2) > 3 {print $2}' | head -3
